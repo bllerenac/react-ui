cd79a09a974f346926a5db8159ab484b522b67d0
Diego <diegoalejandro1094@gmail.com>
2021-02-18 21:58:07 -0500

    Adding the library for icons and initializing the js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import Navbar__links from "../components/navbar_links"
+import Navbar__icons from "../components/navbar_icons"
 
 const Header = () =>{
   return (
     <header className="header">
       <Navbar__links />
+      <Navbar__icons />
     </header>
   );
 };
---
+++ src/components/navbar_icons.js
@@ -0,0 +1,6 @@
+import "remixicon/fonts/remixicon.css";
+import "../stylesheets/navbar_icons.css";
+
+const Navbar__links = () =>{
+  
+}
