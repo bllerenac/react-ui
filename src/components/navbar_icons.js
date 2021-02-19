5e6e51f1f0835c299e207ca7b5c3598d23f74979
Diego <diegoalejandro1094@gmail.com>
2021-02-18 22:13:33 -0500

    Adding the icons to the navbar__icons-js

--- src/components/navbar_icons.js
+++ src/components/navbar_icons.js
@@ -1,6 +1,12 @@
 import "remixicon/fonts/remixicon.css";
 import "../stylesheets/navbar_icons.css";
 
-const Navbar__links = () =>{
-  
+const Navbar__icons= () =>{
+  <div className="navbar_icons">
+    <i class="ri-notification-3-fill"></i>
+    <i class="ri-chat-3-fill"></i>
+    <i class="ri-user-fill"></i>
+  </div>
 }
+
+export default Navbar__icons;
