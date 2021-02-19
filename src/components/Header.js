6eb0c2c81d418a28e5add74d8f61db07570e5312
Diego <diegoalejandro1094@gmail.com>
2021-02-18 22:43:23 -0500

    Adding the styles on navbar_icons and now are set on the navbar_icons.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import Navbar__links from "../components/navbar_links"
 import Navbar__icons from "../components/navbar_icons"
+import "../stylesheets/header.css"
 
 const Header = () =>{
   return (
     <header className="header">
       <Navbar__links />
+      {/* Search bar */}
       <Navbar__icons />
     </header>
   );
--- src/components/navbar_icons.js
+++ src/components/navbar_icons.js
@@ -2,11 +2,11 @@ import "remixicon/fonts/remixicon.css";
 import "../stylesheets/navbar_icons.css";
 
 const Navbar__icons= () =>{
-  <div className="navbar_icons">
-    <i class="ri-notification-3-fill"></i>
-    <i class="ri-chat-3-fill"></i>
-    <i class="ri-user-fill"></i>
-  </div>
+  return (<div className="navbar_icons">
+    <i class="ri-notification-3-fill navbar__icon"></i>
+    <i class="ri-chat-3-fill navbar__icon"></i>
+    <i class="ri-user-fill navbar__icon"></i>
+  </div>);
 }
 
 export default Navbar__icons;
